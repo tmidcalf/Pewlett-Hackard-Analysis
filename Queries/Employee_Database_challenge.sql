a9b76d03858cf801067a4a2af0b9344fc846ee69
tmidcalf <tylermidcalf@gmail.com>
2022-05-10 18:56:38 -0700

    Final challenge documents

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,51 @@
+-- Create Retirement Titles table
+SELECT 	   e.emp_no,
+	 	   e.first_name,
+		   e.last_name,
+	 	   ti.title,
+	 	   ti.from_date,
+		   ti.to_date
+INTO 	   retirement_titles
+FROM 	   employees AS e
+INNER JOIN titles AS ti
+ON 		   (e.emp_no = ti.emp_no)
+WHERE 	   e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY   emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT  DISTINCT ON (emp_no) 
+		emp_no,
+		first_name,
+		last_name,
+		title
+INTO 	unique_titles
+FROM 	retirement_titles
+WHERE 	to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Create a table of counts by each job title
+SELECT 	 COUNT(emp_no),
+		 title
+INTO 	 retiring_titles
+FROM 	 unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+-- Create Mentorship Eligibilty table
+SELECT 	   DISTINCT ON (e.emp_no)
+		   e.emp_no,
+	 	   e.first_name,
+		   e.last_name,
+		   e.birth_date,
+		   de.from_date,
+		   de.to_date,
+		   ti.title
+INTO 	   mentorship_eligibilty
+FROM 	   employees AS e
+INNER JOIN dept_emp AS de
+ON 		   (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON		   (e.emp_no = ti.emp_no)
+WHERE 	   (de.to_date = '9999-01-01')
+		   AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY   emp_no ASC;
--- schema.sql
+++ schema.sql
@@ -44,7 +44,7 @@ CREATE TABLE titles (
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+  	PRIMARY KEY (emp_no, title, from_date)
 );
 
 -- Drop table if exists
@@ -56,12 +56,5 @@ CREATE TABLE dept_emp (
  	to_date DATE NOT NULL,
  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  	PRIMARY KEY (emp_no)
+  	PRIMARY KEY (emp_no, dept_no)
 );
-
-SELECT * FROM departments;
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM salaries;
-SELECT * FROM titles;
-SELECT * FROM dept_emp;
